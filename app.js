64a5dd0f14c504f86d38e060b0d29e315faf6853
rkiel <cygnus@pobox.com>
2014-12-22 08:37:22 -0500

    added express

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    express  = require('express');
 
 mongoose.connect('mongodb://192.168.33.30', function(err) {
     if (err) throw err;
     console.log("connnected!");
-    mongoose.disconnect();
+
+    var app = express();
+    app.get('/', function(req,res) {
+      res.send(200, 'hello mongoose blog');
+    });
+
+    app.listen(3000, function() {
+      console.log('now listening on http://192.168.33.60:3000');
+    });
 });
